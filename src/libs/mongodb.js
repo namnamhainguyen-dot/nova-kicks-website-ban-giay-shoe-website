d0c4a12ca468a08f416f085809d5d8214b913939
namnamhainguyen-dot <namnamhainguyen@gmail.com>
2026-08-01 17:52:31 +0700

    fix mongodb

--- src/libs/mongodb.js
+++ src/libs/mongodb.js
@@ -1,7 +1,8 @@
 import { MongoClient } from 'mongodb';
 
-const uri = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/?directConnection=true';
+const uri = process.env.MONGODB_URI;
 const dbName = process.env.MONGODB_DB || 'Nova-kicks';
+
 const options = {
   serverSelectionTimeoutMS: 5000,
 };
@@ -9,13 +10,19 @@ const options = {
 let client;
 let clientPromise;
 
+if (!process.env.MONGODB_URI) {
+  throw new Error('Vui lòng thêm biến MONGODB_URI vào file .env.local hoặc Vercel Environment Variables');
+}
+
 if (process.env.NODE_ENV === 'development') {
+  // Trong môi trường dev, dùng biến global để giữ kết nối qua các lần HMR (Hot Module Replacement)
   if (!global._mongoClientPromise) {
     client = new MongoClient(uri, options);
     global._mongoClientPromise = client.connect();
   }
   clientPromise = global._mongoClientPromise;
 } else {
+  // Trong môi trường production (Vercel)
   client = new MongoClient(uri, options);
   clientPromise = client.connect();
 }
